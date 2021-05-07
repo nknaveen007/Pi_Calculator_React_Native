d9b55fe80154e248e3161a74688be459a5d96cf8
nknaveen007 <connaveen007@gmail.com>
2021-05-07 07:47:56 -0700

    Pi_Decimal_Value_Calculator_React_Native_Expo

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainScreen from './Screen/MainScreen';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+
+const App=()=> {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name='Pi' component={MainScreen}/>
+     
+      </Stack.Navigator>
+    </NavigationContainer>
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const main=()=>{
+  return(
+   
+     <App/>
+  
+  )
+}
+
+
+export default main;
